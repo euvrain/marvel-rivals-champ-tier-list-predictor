1a0aca4fbb6961ed4d71c579c8168140e6686b67
euvrain <taylormcdoc@gmail.com>
2026-06-26 19:33:12 -0400

    feat: add initial imports and setup for model training

---
+++ model.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import numpy as np
+
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
+from sklearn.preprocessing import LabelEncoder
+
+features=[]
+
+
