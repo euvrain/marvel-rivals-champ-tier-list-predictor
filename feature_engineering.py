19b006265306913d976619dfa0c970f821884f2a
euvrain <taylormcdoc@gmail.com>
2026-06-26 19:53:42 -0400

    feat: add kda, winrate, and additional feature calculations in feature engineering

--- feature_engineering.py
+++ feature_engineering.py
@@ -6,15 +6,15 @@ data=pd.read_csv("raw_hero_stats.csv")
 print(data.head())
 feature_cols=[]
 #kda and winrate
-winrate=data['wins']/data['matches']
+data['winrate']=data['wins']/data['matches']
 
-kda=0
-print(winrate.head())
 
-kda=data['k']/(data['d']).replace(0,1)
-print(kda.head())
-feature_cols.append(kda)
-feature_cols.append(winrate)
+print(data['winrate'].head())
+
+data['kda']=data['k']/(data['d']).replace(0,1)
+print(data['kda'].head())
+feature_cols.append('kda')
+feature_cols.append('winrate')
 #damage per min
 
 split=data['play_time'].str.extract(r'(?P<hours>\d+)h\s*(?P<minutes>\d+)m\s*(?P<seconds>\d+)s')
@@ -34,30 +34,30 @@ print(split.head())
 data["play_time_minutes"]=split['total_minutes']
 data['dmg_per_min'] = data['total_hero_damage']/data['play_time_minutes']
 print(data['dmg_per_min'].head())
-feature_cols.append(data['dmg_per_min'])
+feature_cols.append('dmg_per_min')
 
 #healing per min
 data['heal_per_min']=data['total_hero_heal']/data['play_time_minutes']
 print(data['heal_per_min'].head())
-feature_cols.append(data['heal_per_min'])
+feature_cols.append('heal_per_min')
 #damage taken per min
 data['dmg_taken_per_min']=data['total_damage_taken']/data['play_time_minutes']
 print(data['dmg_taken_per_min'].head())
-feature_cols.append(data['dmg_taken_per_min'])
+feature_cols.append('dmg_taken_per_min')
 
 #damage per match
 data['dmg_per_match']=data['total_hero_damage']/data['matches']
-feature_cols.append(data['dmg_per_match'])
+feature_cols.append('dmg_per_match')
 #healing per match 
 data['heal_per_match']=data['total_hero_heal']/data['matches']
-feature_cols.append(data['heal_per_match'])
+feature_cols.append('heal_per_match')
 #damage taken per match
 data['dmg_taken_per_match']=data['total_damage_taken']/data['matches']
-feature_cols.append(data['dmg_taken_per_match'])
+feature_cols.append('dmg_taken_per_match')
 #hitrate
 data['hit_rate']=data['session_hit_rate']
 
-feature_cols.append(data['hit_rate'])
+feature_cols.append('hit_rate')
 
 X=data[feature_cols]
 X.to_csv("features.csv", index=False)
