14d781638e1987ed1c6c186a765c7aec80c7c2e8
euvrain <taylormcdoc@gmail.com>
2026-06-26 19:38:55 -0400

    feat: load hero stats from CSV and update feature extraction process

--- model.py
+++ model.py
@@ -5,7 +5,10 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report
 from sklearn.preprocessing import LabelEncoder
+from feature_engineering import features
 
-features=[]
+data=pd.read_csv("raw_hero_stats.csv")
+data=data.columns(features)
+print(data)
 
 
