fd0dfd23111e51eb54164d14defcb6e6c6913fef
euvrain <taylormcdoc@gmail.com>
2026-06-27 13:19:10 -0400

    feat: update hero name formatting and add URL encoding for API requests

--- collect.py
+++ collect.py
@@ -3,6 +3,7 @@ import pandas as pd
 import time
 from dotenv import load_dotenv
 import os
+import urllib.parse
 
 load_dotenv()
 API_KEY = os.getenv("API_KEY")
@@ -10,10 +11,11 @@ BASE_URL = "https://marvelrivalsapi.com/api/v1"
 HEADERS = {"x-api-key": API_KEY}
 
 # First get all hero names
+
 heroes_resp = requests.get(f"{BASE_URL}/heroes", headers=HEADERS)
 heroes = heroes_resp.json()  # list of hero objects
 heroes=pd.DataFrame(heroes)
-heroes['name'] = heroes['name'].str.replace(' ', '-').str.lower()
+heroes['name'] = heroes['name'].str.lower()
 print(heroes.columns)
 hero_names=heroes['name']
 
@@ -34,13 +36,14 @@ heroes.to_csv("raw_heros.csv", index=False)
 # df.to_csv("raw_hero_stats.csv", index=False)
 records = []
 for name in hero_names:
+    encoded_name = urllib.parse.quote(name) 
     stats_resp = requests.get(f"{BASE_URL}/heroes/hero/{name}/stats", headers=HEADERS)
     
     if stats_resp.status_code == 200:
         records.append(stats_resp.json())
         print(f"✓ {name}")
     elif stats_resp.status_code == 429:
-        print(f"⏳ rate limited on {name}, waiting 60s...")
+        print(f" rate limited on {name}, waiting 60s...")
         time.sleep(60)
         retry = requests.get(f"{BASE_URL}/heroes/hero/{name}/stats", headers=HEADERS)
         if retry.status_code == 200:
